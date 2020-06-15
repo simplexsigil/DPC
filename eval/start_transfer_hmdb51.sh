6305bada063436739b448f763078a9e4bc7228a3
David Schneider - Betr. Saquib Sarfraz <david.schneider2@student.kit.edu>
2020-06-15 23:08:58 +0200

    Changes on test scripts.

--- eval/start_transfer_hmdb51.sh
+++ eval/start_transfer_hmdb51.sh
@@ -1,12 +1,28 @@
-mkdir -p log_files
+mkdir -p log_files/transfer
 
-log_file=log_files/pretrained_nturgbd_transfer_hmdb51_all_splits.log
-log_file_training=log_files/pretrained_nturgbd_transfer_hmdb51_all_splits_training.log
-pretrained_net="../dpc/log_tmp/nturgbd-128_r18_dpc-rnn_bs96_lr0.001_seq6_pred2_len5_ds1_train-all/model/model_best_epoch85.pth.tar"
+log_file=log_files/transfer/pretrained_nturgbd_cont_transfer_hmdb51_all_splits.log
+log_file_training=log_files/transfer/pretrained_nturgbd_cont_transfer_hmdb51_all_splits_training.log
+pretrained_net="/home/dschneider/workspace/DPC/dpc/20200613221038_training_skelcont/nturgbd-128_r18_skelcont_bs40_len30_ds1_train-all/model/model_best_epoch44.pth.tar"
 
-echo "Start of transfer training on HMDB51 split 2:" > $log_file
+dataset="hmdb51"
+seq_len=30
+num_seq=1
+train_what="ft"
+batch_size=20
+num_workers=16
+gpus="0 3"
+
+echo "Start of transfer training on HMDB51 split 1:" > $log_file
 date >> $log_file
-python3 test.py --prefix pt_nturgbd_eval_hmdb51_sp_2_ep_200_ft --dataset hmdb51 --split 2 --train_what ft --epochs 200 --seq_len 5 --num_seq 6 --ds 1 --batch_size 96 --num_workers 32 --gpu 4 5 --pretrain $pretrained_net > $log_file_training
+python3 test.py --prefix pt_nturgbd_cont_transfer_hmdb51_sp_1_ep_200_ft --dataset $dataset --split 1 --train_what $train_what --epochs 200 --seq_len $seq_len --num_seq $num_seq --ds 1 --batch_size $batch_size --num_workers $num_workers --gpu $gpus --pretrain $pretrained_net > $log_file_training
+echo "Finished evaluation training on HMDB51 split 1 with exit code $?" >> $log_file
+date >> $log_file
+
+echo "" >> $log_file
+
+echo "Start of evaluation training on HMDB51 split 2:" >> $log_file
+date >> $log_file
+python3 test.py --prefix pt_nturgbd_cont_transfer_hmdb51_sp_2_ep_200_ft --dataset $dataset --split 2 --train_what $train_what --epochs 200 --seq_len $seq_len --num_seq $num_seq --ds 1 --batch_size $batch_size --num_workers $num_workers --gpu $gpus --pretrain $pretrained_net >> $log_file_training
 echo "Finished evaluation training on HMDB51 split 2 with exit code $?" >> $log_file
 date >> $log_file
 
@@ -14,14 +30,6 @@ echo "" >> $log_file
 
 echo "Start of evaluation training on HMDB51 split 3:" >> $log_file
 date >> $log_file
-python3 test.py --prefix pt_nturgbd_eval_hmdb51_sp_3_ep_200_ft --dataset hmdb51 --split 3 --train_what ft --epochs 200 --seq_len 5 --num_seq 6 --ds 1 --batch_size 96 --num_workers 32 --gpu 4 5 --pretrain $pretrained_net >> $log_file_training
+python3 test.py --prefix pt_nturgbd_cont_transfer_hmdb51_sp_3_ep_200_ft --dataset $dataset --split 3 --train_what $train_what --epochs 200 --seq_len $seq_len --num_seq $num_seq --ds 1 --batch_size $batch_size --num_workers $num_workers --gpu $gpus --pretrain $pretrained_net >> $log_file_training
 echo "Finished evaluation training on HMDB51 split 3 with exit code $?" >> $log_file
-date >> $log_file
-
-echo "" >> $log_file
-
-echo "Start of evaluation training on HMDB51 split 1:" >> $log_file
-date >> $log_file
-python3 test.py --prefix pt_nturgbd_eval_hmdb51_sp_1_ep_200_ft --dataset hmdb51 --split 1 --train_what ft --epochs 200 --seq_len 5 --num_seq 6 --ds 1 --batch_size 96 --num_workers 32 --gpu 4 5 --pretrain $pretrained_net >> $log_file_training
-echo "Finished evaluation training on HMDB51 split 1 with exit code $?" >> $log_file
 date >> $log_file
