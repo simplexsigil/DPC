cd032ee0529bad9ff5ef614260919ba91b805fa2
David Schneider <simplexsigil@gmail.com>
2020-06-18 13:20:21 +0200

    Added more pairwise distance functions for score calculation (l2 and cosine distance).

--- dpc/model_3d.py
+++ dpc/model_3d.py
@@ -107,9 +107,67 @@ class SkeleContrast(nn.Module):
         pred_rgb = pred_rgb.contiguous() # .view(B*N, D)
         pred_sk = pred_sk.contiguous() # .view(B*N, D)
 
-        score = torch.matmul(pred_sk, pred_rgb.transpose(0, 1))
+        # score = torch.matmul(pred_sk, pred_rgb.transpose(0, 1))
+        #score = self.pairwise_euc_dist(pred_sk, pred_rgb)
+        score = -self.pairwise_distances(x=pred_sk, y=pred_rgb, matching_fn="dot")
+
 
         return score
+
+    def pairwise_euc_dist(self, x1, x2):
+        s = torch.sum((x1 * x2), dim=1)
+
+        na = torch.reshape(s, [-1, 1])
+        nb = torch.reshape(s, [1, -1])
+
+        score = torch.matmul(x1, x2.transpose(0, 1))
+        d = nn.functional.relu(na - 2 * score + nb)
+        '''
+        mask = K.cast(K.equal(d, 0.0), dtype='float32')
+        d = d + mask * K.epsilon()
+        d = K.sqrt(d)
+        d = d * (1.0 - mask)
+        '''
+        return -d
+
+    def pairwise_distances(self,
+                           x: torch.Tensor,
+                           y: torch.Tensor,
+                           matching_fn: str) -> torch.Tensor:
+        """Efficiently calculate pairwise distances (or other similarity scores) between
+        two sets of samples.
+        # Arguments
+            x: Query samples. A tensor of shape (n_x, d) where d is the embedding dimension
+            y: Class prototypes. A tensor of shape (n_y, d) where d is the embedding dimension
+            matching_fn: Distance metric/similarity score to compute between samples
+        """
+        n_x = x.shape[0]
+        n_y = y.shape[0]
+
+        eps = 1e-7
+
+        if matching_fn == 'l2':
+            distances = (
+                    x.unsqueeze(1).expand(n_x, n_y, -1) -
+                    y.unsqueeze(0).expand(n_x, n_y, -1)
+            ).pow(2).sum(dim=2)
+            return distances
+        elif matching_fn == 'cosine':
+            normalised_x = x / (x.pow(2).sum(dim=1, keepdim=True).sqrt() + eps)
+            normalised_y = y / (y.pow(2).sum(dim=1, keepdim=True).sqrt() + eps)
+
+            expanded_x = normalised_x.unsqueeze(1).expand(n_x, n_y, -1)
+            expanded_y = normalised_y.unsqueeze(0).expand(n_x, n_y, -1)
+
+            cosine_similarities = (expanded_x * expanded_y).sum(dim=2)
+            return 1 - cosine_similarities
+        elif matching_fn == 'dot':
+            expanded_x = x.unsqueeze(1).expand(n_x, n_y, -1)
+            expanded_y = y.unsqueeze(0).expand(n_x, n_y, -1)
+
+            return -(expanded_x * expanded_y).sum(dim=2)
+        else:
+            raise (ValueError('Unsupported similarity function'))
 
     def _initialize_weights(self, module):
         for name, param in module.named_parameters():
