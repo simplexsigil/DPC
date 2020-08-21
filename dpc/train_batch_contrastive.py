ca5c3a693f3e0b8ead696163d2f636a4c845e7e0
David Schneider <simplexsigil@gmail.com>
2020-08-21 02:04:28 +0200

    Fixed iteration time printing.

--- dpc/train_batch_contrastive.py
+++ dpc/train_batch_contrastive.py
@@ -147,7 +147,7 @@ def train_skvid_batch_contrast(data_loader, model, optimizer, criterion, epoch, 
         if idx % args.print_freq == 0:
             e_tic = time.perf_counter()
             write_stats_batch_contrast_iteration(tr_stats, writer_train, iteration)
-            print_tr_stats_loc_avg(tr_stats, epoch, idx, len(data_loader), e_tic - tic)
+            print_tr_stats_loc_avg(tr_stats, epoch, idx, len(data_loader), tr_stats["time_all"].local_avg)
 
         iteration += 1
 
